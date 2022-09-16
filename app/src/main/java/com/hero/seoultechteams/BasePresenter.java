ec582f74121871135d7593321ea0d581cc886a5a
thsamajiki <ant1102@naver.com>
2022-09-16 18:11:18 +0900

    test5

---
+++ app/src/main/java/com/hero/seoultechteams/BasePresenter.java
@@ -0,0 +1,5 @@
+package com.hero.seoultechteams;
+
+public interface BasePresenter {
+    void start();
+}
---
+++ app/src/main/java/com/hero/seoultechteams/presenter/TasksFilterType.java
@@ -0,0 +1,18 @@
+package com.hero.seoultechteams.presenter;
+
+public enum TasksFilterType {
+    /**
+     * Do not filter tasks.
+     */
+    ALL_TASKS,
+
+    /**
+     * Filters only the active (not completed yet) tasks.
+     */
+    ACTIVE_TASKS,
+
+    /**
+     * Filters only the completed tasks.
+     */
+    COMPLETED_TASK
+}
