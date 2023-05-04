1f43add7fec0f9e31d1e29e14fd3a0a46017b13a
thsamajiki <ant1102@naver.com>
2023-05-05 01:45:35 +0900

    탈퇴 기능 추가

--- app/src/main/java/com/hero/seoultechteams/view/main/account/setting/presenter/SettingPresenter.java
+++ app/src/main/java/com/hero/seoultechteams/view/main/account/setting/presenter/SettingPresenter.java
@@ -1,20 +1,55 @@
 package com.hero.seoultechteams.view.main.account.setting.presenter;
 
-import android.content.Context;
+import android.util.Log;
 
-import com.hero.seoultechteams.domain.setting.usecase.GetSettingUseCase;
+import com.hero.seoultechteams.domain.common.OnCompleteListener;
+import com.hero.seoultechteams.domain.common.OnFailedListener;
+import com.hero.seoultechteams.domain.user.entity.UserEntity;
+import com.hero.seoultechteams.domain.user.usecase.GetAccountProfileUseCase;
+import com.hero.seoultechteams.domain.user.usecase.RemoveUserUseCase;
 import com.hero.seoultechteams.view.main.account.setting.contract.SettingContract;
 
 public class SettingPresenter implements SettingContract.Presenter {
     private final SettingContract.View view;
-    private final GetSettingUseCase getSettingUseCase;
+//    private final GetSettingUseCase getSettingUseCase;
+    private GetAccountProfileUseCase getAccountProfileUseCase;
+    private final RemoveUserUseCase removeUserUseCase;
 
-    public SettingPresenter(SettingContract.View view, GetSettingUseCase getSettingUseCase) {
+    public SettingPresenter(SettingContract.View view, GetAccountProfileUseCase getAccountProfileUseCase, RemoveUserUseCase removeUserUseCase) {
         this.view = view;
-        this.getSettingUseCase = getSettingUseCase;
+        this.getAccountProfileUseCase = getAccountProfileUseCase;
+        this.removeUserUseCase = removeUserUseCase;
     }
 
     public void getSettingFromDatabase() {
 
+    }
+
+    @Override
+    public UserEntity getUserEntity() {
+        return getAccountProfileUseCase.invoke();
+    }
+
+    @Override
+    public void dropOut(UserEntity userEntity) {
+        removeUserUseCase.invoke(new OnCompleteListener<UserEntity>() {
+            @Override
+            public void onComplete(boolean isSuccess, UserEntity data) {
+                if (isSuccess) {
+                    view.onDropOut();
+                    Log.d("SettingPresenter", "onComplete: remove success");
+                } else {
+                    view.failedDropOut();
+                    Log.d("SettingPresenter", "onComplete: remove failed");
+                }
+            }
+        }, new OnFailedListener() {
+            @Override
+            public void onFailed(Exception exception) {
+                exception.getCause();
+                view.failedDropOut();
+                Log.d("SettingPresenter", "OnFailedListener: remove failed");
+            }
+        }, userEntity);
     }
 }
