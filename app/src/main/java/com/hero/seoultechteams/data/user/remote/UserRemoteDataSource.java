3758e972d8de76b8b7f48c190771d72eac1ab830
thsamajiki <ant1102@naver.com>
2023-05-05 01:44:45 +0900

    탈퇴 기능 추가

--- app/src/main/java/com/hero/seoultechteams/data/user/remote/UserRemoteDataSource.java
+++ app/src/main/java/com/hero/seoultechteams/data/user/remote/UserRemoteDataSource.java
@@ -1,17 +1,17 @@
 package com.hero.seoultechteams.data.user.remote;
 
-import com.hero.seoultechteams.domain.common.OnCompleteListener;
 import com.hero.seoultechteams.database.member.entity.MemberData;
 import com.hero.seoultechteams.database.team.entity.TeamData;
 import com.hero.seoultechteams.database.user.entity.UserData;
+import com.hero.seoultechteams.domain.common.OnCompleteListener;
 import com.hero.seoultechteams.domain.common.OnFailedListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface UserRemoteDataSource {
     void addUser(OnCompleteListener<UserData> onCompleteListener, OnFailedListener onFailedListener, String userName, String email, String pwd);
     void updateUser(OnCompleteListener<UserData> onCompleteListener, UserData userData);
+    void removeUser(OnCompleteListener<UserData> onCompleteListener, OnFailedListener onFailedListener, UserData userData);
 
     void addUserList(OnCompleteListener<List<UserData>> onCompleteListener, TeamData teamData, List<UserData> userDataList, List<MemberData> memberDataList);
 
