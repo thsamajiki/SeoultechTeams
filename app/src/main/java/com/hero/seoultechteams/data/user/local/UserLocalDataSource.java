beadd1e305c712f8bda074fc5caf9046aab7ef95
thsamajiki <ant1102@naver.com>
2023-05-05 01:45:01 +0900

    탈퇴 기능 추가

--- app/src/main/java/com/hero/seoultechteams/data/user/local/UserLocalDataSource.java
+++ app/src/main/java/com/hero/seoultechteams/data/user/local/UserLocalDataSource.java
@@ -1,13 +1,8 @@
 package com.hero.seoultechteams.data.user.local;
 
-import com.hero.seoultechteams.database.member.entity.MemberData;
-import com.hero.seoultechteams.database.team.entity.TeamData;
-import com.hero.seoultechteams.domain.common.OnCompleteListener;
 import com.hero.seoultechteams.database.user.entity.UserData;
+import com.hero.seoultechteams.domain.common.OnCompleteListener;
 import com.hero.seoultechteams.domain.common.OnFailedListener;
-import com.hero.seoultechteams.domain.user.entity.UserEntity;
-
-import java.util.List;
 
 public interface UserLocalDataSource {
     void getData(OnCompleteListener<UserData> onCompleteListener, String userKey);
@@ -16,7 +11,7 @@ public interface UserLocalDataSource {
 
     void updateUser(final OnCompleteListener<UserData> onCompleteListener, UserData userData);
 
-    void removeUser(final OnCompleteListener<UserData> onCompleteListener, UserData userData);
+    void removeUser(final OnCompleteListener<UserData> onCompleteListener, OnFailedListener onFailedListener, UserData userData);
 
     void signOut();
 }
