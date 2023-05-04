376e64e7c36edb77b3883ef7afa6f281d41a4dbe
thsamajiki <ant1102@naver.com>
2023-05-05 01:45:41 +0900

    탈퇴 기능 추가

--- app/src/main/java/com/hero/seoultechteams/view/main/account/setting/contract/SettingContract.java
+++ app/src/main/java/com/hero/seoultechteams/view/main/account/setting/contract/SettingContract.java
@@ -1,13 +1,23 @@
 package com.hero.seoultechteams.view.main.account.setting.contract;
 
+import com.hero.seoultechteams.domain.user.entity.UserEntity;
+
 public abstract class SettingContract {
     public interface View {
         void onGetSetting();
 
         void failedGetSetting();
+
+        void onDropOut();
+
+        void failedDropOut();
     }
 
     public interface Presenter {
         void getSettingFromDatabase();
+
+        UserEntity getUserEntity();
+
+        void dropOut(UserEntity userEntity);
     }
 }
