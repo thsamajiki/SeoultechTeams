5a902c2c6d6fc5cdde052855dc46c697f5e51fab
thsamajiki <ant1102@naver.com>
2023-07-14 00:45:41 +0900

    BaseActivity 수정

--- app/src/main/java/com/hero/seoultechteams/BaseActivity.java
+++ app/src/main/java/com/hero/seoultechteams/BaseActivity.java
@@ -1,12 +1,31 @@
 package com.hero.seoultechteams;
 
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewbinding.ViewBinding;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+public abstract class BaseActivity<VB extends ViewBinding> extends AppCompatActivity {
 
-public class BaseActivity extends AppCompatActivity {
+    protected abstract VB getViewBinding();
+    protected VB binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initBinding();
+    }
+
+    private void initBinding(){
+        binding = getViewBinding();
+        View view = binding.getRoot();
+        setContentView(view);
+    }
 
     public FirebaseUser getCurrentUser() {
         return FirebaseAuth.getInstance().getCurrentUser();
