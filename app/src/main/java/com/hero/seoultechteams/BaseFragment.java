f8158b25b8c99bd02dceb3feddf9f2967d88b34a
thsamajiki <ant1102@naver.com>
2023-07-17 01:42:43 +0900

    BaseFragment 적용

--- app/src/main/java/com/hero/seoultechteams/BaseFragment.java
+++ app/src/main/java/com/hero/seoultechteams/BaseFragment.java
@@ -1,12 +1,41 @@
 package com.hero.seoultechteams;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewbinding.ViewBinding;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
-public class BaseFragment extends Fragment {
+//public class BaseFragment extends Fragment {
+//    public FirebaseUser getCurrentUser() {
+//        return FirebaseAuth.getInstance().getCurrentUser();
+//    }
+//}
+
+public abstract class BaseFragment<VB extends ViewBinding> extends Fragment {
+
+    @NonNull
+    protected abstract VB getViewBinding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container);
+    protected VB binding;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        binding = getViewBinding(inflater, container);
+
+        return binding.getRoot();
+    }
+
     public FirebaseUser getCurrentUser() {
         return FirebaseAuth.getInstance().getCurrentUser();
     }
